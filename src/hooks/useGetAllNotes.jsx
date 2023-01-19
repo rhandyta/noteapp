005bb348e3e45ebf3ffb7600dc1b1bbb675efdf9
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-19 23:20:47 +0700

    hot fix auto fetch moment logout

--- src/hooks/useGetAllNotes.jsx
+++ src/hooks/useGetAllNotes.jsx
@@ -5,39 +5,36 @@ import { toastError, toastSuccess } from "../components/Toast";
 function useGetAllNotes({ setIsLoading }) {
     const auth = useSelector((state) => state.user);
     const [notes, setNotes] = useState([]);
+    const getAllNote = async () => {
+        try {
+            await setIsLoading(true);
+            await fetch(`${import.meta.env.VITE_API_URL}notes?page=1`, {
+                method: "GET",
+                headers: {
+                    Authorization: `${auth.type} ${auth.token}`,
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                },
+            }).then(async (res) => {
+                let tmpNotes = [];
+                let { notes } = await res.json();
+                if (notes.data.length > 0) {
+                    tmpNotes = notes.data.map((note) => note);
+                    setNotes(tmpNotes);
+                    return await setIsLoading(false);
+                }
+                await setIsLoading(false);
+                return toastSuccess("Note not yet available");
+            });
+        } catch (error) {
+            await setIsLoading(false);
+            return toastError("Something went wrong");
+        }
+    };
 
     useEffect(() => {
-        const getAllNote = async () => {
-            try {
-                await setIsLoading(true);
-                await fetch(`${import.meta.env.VITE_API_URL}notes?page=1`, {
-                    method: "GET",
-                    headers: {
-                        Authorization: `${auth.type} ${auth.token}`,
-                        "Content-Type": "application/json",
-                        Accept: "application/json",
-                        "Access-Control-Allow-Origin": "*",
-                    },
-                }).then(async (res) => {
-                    let tmpNotes = [];
-                    let { notes } = await res.json();
-                    if (notes.data.length > 0) {
-                        tmpNotes = notes.data.map((note) => note);
-                        setNotes(tmpNotes);
-                        return await setIsLoading(false);
-                    }
-                    await setIsLoading(false);
-                    return toastSuccess("Note not yet available");
-                });
-            } catch (error) {
-                await setIsLoading(false);
-                return toastError("Something went wrong");
-            }
-        };
-
-        return () => {
-            getAllNote();
-        };
+        getAllNote();
     }, []);
     return notes;
 }
