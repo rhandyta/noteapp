c88d8735ac120eddb693131704c40a9921f5a6e9
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-26 03:59:27 +0700

    Search

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,14 +9,30 @@ import Skeleton from "../components/Skeleton";
 import { toastError, toastSuccess } from "../components/Toast";
 import { useSelector } from "react-redux";
 import Modal from "../components/Modal";
+import { useRef } from "react";
 
 function Dashboard() {
     const auth = useSelector((auth) => auth.user);
+    const searchRef = useRef(null);
     const [isLoading, setIsLoading] = useState(false);
     const [loadedPages, setLoadedPages] = useState([]);
     const getAllNotes = useGetAllNotes({ setIsLoading });
     const [allNotes, setAllNotes] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
+    const initialValues = {
+        title: "",
+        body: "",
+        visible: 0,
+        archive: 0,
+    };
+    const initialValueSearch = {
+        title: "",
+    };
+
+    const validationSchema = yup.object({
+        title: yup.string().min(5).max(150).required().trim(),
+    });
+
     let page = 2;
 
     function handleScroll() {
@@ -24,33 +40,76 @@ function Dashboard() {
         const windowPixel = window.innerHeight;
         const height = document.documentElement.scrollHeight;
         if (top + windowPixel + 1 >= height) {
+            console.log(searchRef.current.value === "");
             if (!loadedPages.includes(page)) {
                 setLoadedPages([...loadedPages, page]);
-                fetch(`${import.meta.env.VITE_API_URL}notes?page=${page}`, {
-                    method: "GET",
-                    headers: {
-                        Authorization: `${auth.type} ${auth.token}`,
-                        "Content-Type": "application/json",
-                        Accept: "application/json",
-                        "Access-Control-Allow-Origin": "http://127.0.0.1:5173/",
-                        "Access-Control-Allow-Headers": "*",
-                        "Access-Control-Allow-Credentials": true,
-                    },
-                })
-                    .then(async (res) => {
-                        const newData = [];
-                        let { notes } = await res.json();
-                        notes.data.forEach((n) => {
-                            if (!allNotes.find((note) => note.id === n.id)) {
-                                newData.push(n);
-                            }
-                        });
-                        setAllNotes((oldN) => [...oldN, ...newData]);
+                console.log(page);
+                if (searchRef.current.value === "") {
+                    fetch(`${import.meta.env.VITE_API_URL}notes?page=${page}`, {
+                        method: "GET",
+                        headers: {
+                            Authorization: `${auth.type} ${auth.token}`,
+                            "Content-Type": "application/json",
+                            Accept: "application/json",
+                            "Access-Control-Allow-Origin":
+                                "http://127.0.0.1:5173/",
+                            "Access-Control-Allow-Headers": "*",
+                            "Access-Control-Allow-Credentials": true,
+                        },
                     })
-                    .catch((error) => {
-                        setIsLoading(false);
-                        return toastError(error.message);
-                    });
+                        .then(async (res) => {
+                            const newData = [];
+                            let { notes } = await res.json();
+                            notes.data.forEach((n) => {
+                                if (
+                                    !allNotes.find((note) => note.id === n.id)
+                                ) {
+                                    newData.push(n);
+                                }
+                            });
+                            setAllNotes((oldN) => [...oldN, ...newData]);
+                            setIsLoading(false);
+                        })
+                        .catch((error) => {
+                            setIsLoading(false);
+                            return toastError(error.message);
+                        });
+                } else {
+                    fetch(
+                        `${
+                            import.meta.env.VITE_API_URL
+                        }notes/search?page=${page}`,
+                        {
+                            method: "GET",
+                            headers: {
+                                Authorization: `${auth.type} ${auth.token}`,
+                                "Content-Type": "application/json",
+                                Accept: "application/json",
+                                "Access-Control-Allow-Origin":
+                                    "http://127.0.0.1:5173/",
+                                "Access-Control-Allow-Headers": "*",
+                                "Access-Control-Allow-Credentials": true,
+                            },
+                        }
+                    )
+                        .then(async (res) => {
+                            const newData = [];
+                            let { notes } = await res.json();
+                            notes.data.forEach((n) => {
+                                if (
+                                    !allNotes.find((note) => note.id === n.id)
+                                ) {
+                                    newData.push(n);
+                                }
+                            });
+                            setAllNotes((oldN) => [...oldN, ...newData]);
+                            setIsLoading(false);
+                        })
+                        .catch((error) => {
+                            setIsLoading(false);
+                            return toastError(error.message);
+                        });
+                }
                 page += 1;
             }
         }
@@ -59,17 +118,6 @@ function Dashboard() {
     function handleClose() {
         setIsOpen(!isOpen);
     }
-
-    const initialValues = {
-        title: "",
-        body: "",
-        visible: 0,
-        archive: 0,
-    };
-
-    const validationSchema = yup.object({
-        title: yup.string().min(5).max(150).required().trim(),
-    });
 
     const onSubmit = async (values, props) => {
         try {
@@ -124,8 +172,7 @@ function Dashboard() {
     const handleDelete = async (slug) => {
         try {
             setIsLoading(true);
-
-            fetch(`${import.meta.env.VITE_API_URL}notes/${slug}`, {
+            await fetch(`${import.meta.env.VITE_API_URL}notes/${slug}`, {
                 method: "DELETE",
                 headers: {
                     Authorization: `${auth.type} ${auth.token}`,
@@ -152,6 +199,41 @@ function Dashboard() {
         }
     };
 
+    const handleSearch = async (values) => {
+        page = 1;
+        try {
+            setIsLoading(true);
+            await fetch(
+                `${import.meta.env.VITE_API_URL}notes/search?title=${
+                    values.title
+                }`,
+                {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "http://127.0.0.1:5173/",
+                        "Access-Control-Allow-Headers": "*",
+                        "Access-Control-Allow-Credentials": true,
+                        Authorization: `${auth.type} ${auth.token}`,
+                    },
+                }
+            )
+                .then(async (res) => {
+                    let { notes } = await res.json();
+                    setAllNotes([...notes.data]);
+                    setIsLoading(false);
+                })
+                .catch((error) => {
+                    setIsLoading(false);
+                    return toastError(error.message);
+                });
+        } catch (error) {
+            setIsLoading(false);
+            return toastError(error.message);
+        }
+    };
+
     useEffect(() => {
         if (allNotes.length === 0) {
             setAllNotes(getAllNotes);
@@ -161,6 +243,13 @@ function Dashboard() {
             window.removeEventListener("scroll", handleScroll);
         };
     }, [getAllNotes]);
+
+    useEffect(() => {
+        window.addEventListener("submit", handleSearch);
+        return () => {
+            window.removeEventListener("submit", handleSearch);
+        };
+    }, []);
 
     return (
         <>
@@ -183,7 +272,10 @@ function Dashboard() {
                     className=" p-2 text-white"
                     onClick={() => setIsOpen(!isOpen)}
                 />
-                <Formik>
+                <Formik
+                    initialValues={initialValueSearch}
+                    onSubmit={handleSearch}
+                >
                     {(props) => {
                         return (
                             <Form>
@@ -191,8 +283,13 @@ function Dashboard() {
                                     name="title"
                                     className="w-44 md:w-60 xl:w-96"
                                     placeholder="Search by title"
+                                    innerRef={searchRef}
                                 />
-                                <Button text={"Search"} className="ml-1 p-2" />
+                                <Button
+                                    text={"Search"}
+                                    className="ml-1 p-2"
+                                    type="submit"
+                                />
                             </Form>
                         );
                     }}
